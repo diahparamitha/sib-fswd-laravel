5e162df8b105189ef3bd423f5264cefb6b2d8ca5
diahparamitha <diahparamitha01@gmail.com>
2023-06-12 00:32:28 +0700

    [FIX] code

--- resources/views/layouts/partials-landing/head.blade.php
+++ resources/views/layouts/partials-landing/head.blade.php
@@ -10,17 +10,17 @@
  <meta name="description" content="">
  <meta name="author" content="">
  <!-- bootstrap css -->
- <link rel="stylesheet" type="text/css" href="{{ asset ('css/asset/bootstrap.min.css') }}">
+ <link rel="stylesheet" type="text/css" href="{{ asset ('../css/asset/bootstrap.min.css') }}">
  <!-- style css -->
- <link rel="stylesheet" type="text/css" href="{{ asset ('css/asset/style.css')}}">
+ <link rel="stylesheet" type="text/css" href="{{ asset ('../css/asset/style.css')}}">
  <!-- Responsive-->
- <link rel="stylesheet" href="{{ asset ('css/asset/responsive.css')}}">
+ <link rel="stylesheet" href="{{ asset ('../css/asset/responsive.css')}}">
  <!-- fevicon -->
- <link rel="icon" href="{{ asset ('images/fevicon.png')}}" type="image/gif" />
+ <link rel="icon" href="{{ asset ('../images/fevicon.png')}}" type="image/gif" />
  <!-- font css -->
  <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;800&display=swap" rel="stylesheet">
  <!-- Scrollbar Custom CSS -->
- <link rel="stylesheet" href="{{ asset ('css/asset/jquery.mCustomScrollbar.min.css')}}">
+ <link rel="stylesheet" href="{{ asset ('../css/asset/jquery.mCustomScrollbar.min.css')}}">
  <!-- Tweaks for older IEs-->
  <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css">
  
